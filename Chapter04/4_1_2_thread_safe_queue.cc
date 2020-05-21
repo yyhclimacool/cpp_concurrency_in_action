52ea0d5f8bb86720f1cb1784e14400400bd5dd45
kevin <yyhclimacool@gmail.com>
2020-05-21 08:34:09 +0800

    threadsafe_queue

---
+++ Chapter04/4_1_2_thread_safe_queue.cc
@@ -0,0 +1,34 @@
+#include <mutex>
+#include <condition_variable>
+#include <thread>
+#include <string>
+
+using namespace std;
+
+template<typename T>
+class threadsafe_queue {
+  mutex queue_mutex;
+  condition_variable queue_cond;
+  queue<T> data_queue;
+public:
+  threadsafe_queue() = default;
+  threadsafe_queue(const threadsafe_queue &other) {
+    lock_guard<mutex> gurad(queue_mutex);
+    data_queue = other.data_queue;
+  }
+
+  void push(const T &val) {
+    {
+      lock_guard<mutex> gurad(queue_mutex);
+      data_queue.push(val);
+    }
+    queue_cond.notify_one();
+  }
+
+  void wait_and_pop(T &ret) {
+    unique_lock<mutex> lk(queue_mutex);
+    queue_cond.wait(lk, [this]{ !data_queue.empty(); });
+    ret = data_queue.front();
+    data_queue.pop();
+  }
+};
